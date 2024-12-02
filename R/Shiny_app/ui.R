091b361b2ec5f1f4d4e965da792b9d8f7f86de07
EmilBerntsen-ui <s225051@dtu.dk>
2024-12-02 11:17:06 +0000

    Gender and Race filtration

--- R/Shiny_app/ui.R
+++ R/Shiny_app/ui.R
@@ -21,6 +21,18 @@ ui <- fluidPage(
         choices = c(unique(data$age), "All"),
         selected = "All"
       ),
+      selectInput(
+        "race",
+        "Filter by race:",
+        choices = c(unique(data$race), "All"),
+        selected = "All"
+      ),
+      selectInput(
+        "gender",
+        "Filter by gender",
+        choices = c(unique(data$gender), "All"),
+        selected = "All"
+      ),
       
       # Checkboxes for medications
       checkboxGroupInput(
